943492ae2e1558a91001dcef7221e6749cf13939
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-01 18:42:43 -0300

    feat: exercise 2 complete

--- 33-intro-to-python/exercises.py
+++ 33-intro-to-python/exercises.py
@@ -1,6 +1,6 @@
 # exercise 1
 # create a function that receives two numbers (ints) and returns
-# the bigger
+# the bigger.
 
 def biggerNumber(n1, n2):
     if n1 > n2:
@@ -11,3 +11,17 @@ def biggerNumber(n1, n2):
 
 print(biggerNumber(2, 4))
 print(biggerNumber(15, 9))
+
+# exercise 2
+# create a function that receives a list and returns the arithmetic median.
+
+
+def median(list):
+    result = 0
+    for n in list:
+        result += n
+    return result / len(list)
+
+
+print(median([1, 2, 3, 4, 5, 6]))
+print(median([10, 20, 30]))
