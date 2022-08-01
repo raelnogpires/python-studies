bfa4c15db9c5760e9cefb021b812b2c66e5b8219
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-01 18:26:17 -0300

    feat: exercise 1 complete

---
+++ 33-intro-to-python/exercises.py
@@ -0,0 +1,13 @@
+# exercise 1
+# create a function that receives two numbers (ints) and returns
+# the bigger
+
+def biggerNumber(n1, n2):
+    if n1 > n2:
+        return n1
+    else:
+        return n2
+
+
+print(biggerNumber(2, 4))
+print(biggerNumber(15, 9))
