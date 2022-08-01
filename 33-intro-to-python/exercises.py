87a036ca25c4fcbc9a00709dc1566cf6e063bd24
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-01 18:59:07 -0300

    feat: exercise 3 complete

--- 33-intro-to-python/exercises.py
+++ 33-intro-to-python/exercises.py
@@ -25,3 +25,22 @@ def median(list):
 
 print(median([1, 2, 3, 4, 5, 6]))
 print(median([10, 20, 30]))
+
+# exercise 3
+# create a function that given x (x > 1) value will print a square of asterisks
+# that have x height and x lenght
+
+
+def starsSquare(x):
+    if x < 1:
+        print("the number must be equal or bigger than 1")
+
+    # loop through the rows
+    for i in range(x):
+        # print each one of the starts
+        for j in range(x):
+            print("*", end=" ")
+        print()
+
+
+starsSquare(5)
