11f92dc7c33232a35145b511ea8943e9c3d67687
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-03 12:52:07 -0300

    chore/day3-33: create .py file por exercise 1

---
+++ 33-intro-to-python/day3-tests/exercise1.py
@@ -0,0 +1,5 @@
+# exercise 1
+# create a function that returns a number list with this exceptions:
+# if the number is divisible by 3, it will show "Fizz"
+# if the number is divisible by 5, it will show "Buzz"
+# if the number is divisible by 3 and 5, it will show "FizzBuzz"
