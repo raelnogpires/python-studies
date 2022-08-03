68d336c94e9361ded414daf519f0a96bc355a917
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-03 13:15:21 -0300

    test/day3-33: create unit tests for exercise 1

---
+++ 33-intro-to-python/day3-tests/test_exercise1.py
@@ -0,0 +1,20 @@
+from exercise1 import number_list_or_fizz_buzz
+
+fizz = [1, 2, "Fizz"]
+buzz = [1, 2, "Fizz", 4, "Buzz"]
+fizz_buzz = [
+    1, 2, "Fizz", 4, "Buzz", "Fizz", 7,
+    8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"
+]
+
+
+def test_func_when_appear_just_fizz():
+    assert number_list_or_fizz_buzz(4) == fizz
+
+
+def test_func_when_appear_fizz_and_buzz():
+    assert number_list_or_fizz_buzz(6) == buzz
+
+
+def test_func_when_appear_fizz_buzz_and_fizzbuzz():
+    assert number_list_or_fizz_buzz(16) == fizz_buzz
