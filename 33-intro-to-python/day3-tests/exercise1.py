c3ba2157a4e8548f92620a9ffeeec3c5d85af456
Rael N. Pires <rnogpiresfinancial@gmail.com>
2022-08-03 12:58:24 -0300

    feat/day3-33: exercise 1 complete

--- 33-intro-to-python/day3-tests/exercise1.py
+++ 33-intro-to-python/day3-tests/exercise1.py
@@ -3,3 +3,21 @@
 # if the number is divisible by 3, it will show "Fizz"
 # if the number is divisible by 5, it will show "Buzz"
 # if the number is divisible by 3 and 5, it will show "FizzBuzz"
+
+
+def number_list_or_fizz_buzz(n):
+    result = []
+    for i in range(1, n):
+        if i % 3 == 0 and i % 5 == 0:
+            result.append("FizzBuzz")
+        elif i % 3 == 0:
+            result.append("Fizz")
+        elif i % 5 == 0:
+            result.append("Buzz")
+        else:
+            result.append(i)
+
+    return result
+
+
+print(number_list_or_fizz_buzz(4))
